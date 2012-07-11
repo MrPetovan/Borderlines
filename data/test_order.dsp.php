5b65167f35ab7822e7e00f75224e2f6a9a575535
Benjamin Lorteau <ben.lort@gmail.com>
2012-07-11 11:16:05 -0400

    Cleaning old test files

--- data/test_order.dsp.php
+++
@@ -1,7 +0,0 @@
-<?php
-  if( isset( $html_msg ) ) {
-    echo $html_msg;
-  }
-?>
-<p><a href="<?php echo Page::get_page_url( PAGE_CODE, false, array( 'action' => 'init_db' ) )?>">Initialiser la DB</a></p>
-<p><a href="<?php echo Page::get_page_url( PAGE_CODE, false, array( 'action' => 'test' ) )?>">Lancer le test</a></p>
