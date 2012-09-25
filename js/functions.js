f3d573d2516739f82e48141f5882e05893c81ff3
Benjamin Lorteau <ben.lort@gmail.com>
2012-09-25 16:51:08 -0400

    Add accordion display Add territory windows display on maps

--- js/functions.js
+++ js/functions.js
@@ -1,35 +1,57 @@
 $(document).ready(function() {
-  /*$("map > area").tooltip({
-    track: true,
-    showURL: false,
-    showBody: " | ",
-    fade: 250
-  });*/
-
-  $('div.accordion').accordion({
-    header: "h4",
-    autoHeight: false
+  $map = $('map');
+  $dialog = $('<div class="dialog">\n\
+    <h4>\n\
+      <a><img src="/img/img_html/arrow_move.png" alt="" title="Move"/></a>\n\
+      <span>Dialog</span>\n\
+      <a class="close">[X]</a>\n\
+    </h4>\n\
+    <div class="content">\n\
+      <iframe style="border: 0"></iframe>\n\
+    </div>\n\
+  </div>').mousedown(function (e) {
+    var inst = $(this).data('draggable');
+    inst._mouseStart(e);
+    inst._trigger('start', e);
+    inst._clear();
   });
+  $dialog.find('.close').click(function(){
+    $(this).parent().parent().hide();
+  })
 
-  $("map > area").cluetip({
-    local:true,
-    cursor: 'help',
-    tracking: true,
-    /*cluetipClass: 'jtip',*/
-    topOffset: 10,
-    leftOffset: 40,
-    arrows: true,
-    showTitle: false
-  });
+  $('map area').each(function(){
+    $localdialog = $dialog
+      .clone(true)
+      .attr('id', 'dialog-' + $(this).attr('territory'))
+      .draggable({
+        handle: 'h4',
+        stack: '.dialog',
+        addClasses: false,
+        appendTo: 'body',
+        containment: 'body'
+      });
+    $localdialog
+      .find('.content')
+      .resizable({
+        helper: "ui-resizable-helper"
+      });
+    $localdialog
+      .find('h4 span').text('Showing ' + $(this).attr('title'));
+    $map.after($localdialog);
+  })
 
+  $map.on('click','area',function(e) {
+      $('#dialog-' + $(this).attr('territory'))
+        .css({'left': e.pageX, 'top': e.pageY - $map.position().top})
+        .show()
+        .find('iframe')
+        .attr('src', 'index.php?page=show_territory_ajax&id=' + $(this).attr('territory'));
 
-
-  //Accordion
-  $('.texte_cache').hide();
-  $('.texte_chapo').append('&nbsp;<a href="#" class="readmore">Lire la suite</a>');
-  $('.texte_chapo').children('a').click(function(e) {
-    $(this).parent().next('.texte_cache').show();
-    $(this).remove();
     return false;
+  });
+
+  $('tbody.archive:not(.current) tr:not(.title)').hide();
+  $('.accordion').on('click', 'tr.title', function() {
+    $(this).siblings().toggle();
   });
 });
