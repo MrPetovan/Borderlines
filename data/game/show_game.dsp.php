0239d0b28e2d0e792a3a12a09736f2b058581a92
Benjamin Lorteau <ben.lort@gmail.com>
2012-10-07 17:06:18 -0400

    Add i18n to show_game

--- data/game/show_game.dsp.php
+++ data/game/show_game.dsp.php
@@ -1,69 +1,69 @@
 <?php
-  $PAGE_TITRE = 'Game : Showing "'.$game->name.'"';
+  $PAGE_TITRE = __('Game : Showing "%s"', $game->name);
 
   $creator = Player::instance( $game->created_by );
 ?>
-<h2>Showing "<?php echo $game->name?>"</h2>
+<h2><?php echo __('Showing "%s"', $game->name )?></h2>
 <div class="informations formulaire">
   <p class="field">
-    <span class="libelle">Status</span>
+    <span class="libelle"><?php echo __('Status')?></span>
     <span class="value"><?php echo $game->status_string?></span>
   </p>
   <p class="field">
-    <span class="libelle">Current Turn</span>
+    <span class="libelle"><?php echo __('Current Turn')?></span>
     <span class="value"><?php echo $game->current_turn.'/'.$game->turn_limit?></span>
   </p>
   <p class="field">
-    <span class="libelle">Turn Interval</span>
+    <span class="libelle"><?php echo __('Turn Interval')?></span>
     <span class="value"><?php echo $game->turn_interval?> seconds</span>
   </p>
 <?php if( !$game->started && $game->min_players ) { ?>
   <p class="field">
-    <span class="libelle">Min Players</span>
+    <span class="libelle"><?php echo __('Min Players')?></span>
     <span class="value"><?php echo $game->min_players?></span>
   </p>
 <?php }?>
 <?php if( $game->max_players ) {?>
   <p class="field">
-    <span class="libelle">Max Players</span>
+    <span class="libelle"><?php echo __('Max Players')?></span>
     <span class="value"><?php echo $game->max_players?></span>
   </p>
 <?php }?>
   <p class="field">
-    <span class="libelle">Created</span>
+    <span class="libelle"><?php echo __('Created')?></span>
     <span class="value"><?php echo guess_time($game->created, GUESS_DATETIME_LOCALE)?>
     by <a href="<?php echo get_page_url('show_player', true, array('id' => $game->created_by ) )?>"><?php echo $creator->name?></a></span>
   </p>
 <?php if( $game->started ) {?>
   <p class="field">
-    <span class="libelle">Started</span>
+    <span class="libelle"><?php echo __('Started')?></span>
     <span class="value"><?php echo guess_time($game->started, GUESS_DATETIME_LOCALE)?></span>
   </p>
 <?php }?>
 <?php if( $game->updated && ! $game->ended ) {?>
   <p class="field">
-    <span class="libelle">Updated</span>
+    <span class="libelle"><?php echo __('Updated')?></span>
     <span class="value"><?php echo guess_time($game->updated, GUESS_DATETIME_LOCALE)?></span>
   </p>
 <?php }?>
 <?php if( $game->ended ) {?>
   <p class="field">
-    <span class="libelle">Ended</span>
+    <span class="libelle"><?php echo __('Ended')?></span>
     <span class="value"><?php echo guess_time($game->ended, GUESS_DATETIME_LOCALE)?></span>
   </p>
 <?php }elseif( $game->updated ) { ?>
   <p class="field">
-    <span class="libelle">Next turn</span>
+    <span class="libelle"><?php echo __('Next turn')?></span>
     <span class="value"><?php echo guess_time( $game->updated + $game->turn_interval, GUESS_DATETIME_LOCALE ) ?></span>
   </p>
 <?php }?>
 <?php $world = World::instance($game->world_id);?>
   <p class="field">
-    <span class="libelle">World</span>
+    <span class="libelle"><?php echo __('World')?></span>
     <span class="value"><a href="<?php echo Page::get_url('show_world', array('id' => $world->id))?>"><?php echo $world->name?></a></span>
   </p>
 </div>
-<h3>Players</h3>
+<h3><?php echo __('Players')?></h3>
 <?php
 
   if(count($game_player_list)) {
@@ -71,15 +71,15 @@
 <table>
   <thead>
     <tr>
-      <th>Player</th>
-      <th>Turn Ready</th>
-      <th>Controlled territory</th>
-      <th>Total troops</th>
+      <th><?php echo __('Player')?></th>
+      <th><?php echo __('Turn Ready')?></th>
+      <th><?php echo __('Controlled territory')?></th>
+      <th><?php echo __('Total troops')?></th>
     </tr>
   </thead>
   <tfoot>
     <tr>
-      <td colspan="4"><?php echo count( $game_player_list ).($game->max_players?'/'.$game->max_players:'')?> players</td>
+      <td colspan="4"><?php echo $game->max_players?__('%s/%s players', count( $game_player_list ), $game->max_players):__('%s players', count( $game_player_list ))?></td>
     </tr>
   </tfoot>
   <tbody>
@@ -107,18 +107,18 @@
 <?php
   }else {
     echo '
-<p>No player yet</p>';
+<p>'.__('No player yet').'</p>';
   }
 
   $is_in_a_game = $current_player->get_current_game() != false;
   $is_playing_in = $is_in_a_game || $game->get_game_player_list( $current_player->id );
   if( !$game->started && !$is_playing_in ) {
     echo '
-<p><a href="'.Page::get_page_url(PAGE_CODE, false, array('action' => 'join', 'id' => $game->id)).'">Join this game</a></p>';
+<p><a href="'.Page::get_page_url(PAGE_CODE, false, array('action' => 'join', 'id' => $game->id)).'">'.__('Join this game').'</a></p>';
   }
 
   if( is_admin() ) {
-    echo '<p><a href="'.Page::get_url('admin_game_view', array('id' => $game->id )).'">Manage game</a></p>';
+    echo '<p><a href="'.Page::get_url('admin_game_view', array('id' => $game->id )).'">'.__('Manage game').'</a></p>';
   }
 ?>
-<p><a href="<?php echo get_page_url('game_list')?>">Return to game list</a></p>
+<p><a href="<?php echo get_page_url('game_list')?>"><?php echo __('Return to game list')?></a></p>
