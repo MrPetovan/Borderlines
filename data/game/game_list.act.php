ee1732bd30caf9e5ce40dbcbf9d73dcd7d07ea6a
Benjamin Lorteau <ben.lort@gmail.com>
2012-10-15 06:15:58 -0400

    Add wall to game_list Add create game form Add quit, start, cancel buttons to game_list

--- data/game/game_list.act.php
+++ data/game/game_list.act.php
@@ -7,26 +7,30 @@
 
   $game_mod = Game::instance();
 
-  if(isset($_POST['game_submit']) && is_admin() ) {
-    unset($_POST['game_submit']);
+  if(isset($_POST['game_submit']) ) {
+    if( $current_player->can_create_game() ) {
+      unset($_POST['game_submit']);
 
-    $game_mod->load_from_html_form($_POST, $_FILES);
-    $game_mod->created = time();
-    $game_mod->current_turn = 0;
-    $game_mod->created_by = $current_player->id;
-    $tab_error = $game_mod->check_valid();
+      $game_mod->load_from_html_form($_POST, $_FILES);
+      $game_mod->created = time();
+      $game_mod->current_turn = 0;
+      $game_mod->created_by = $current_player->id;
+      $tab_error = $game_mod->check_valid();
 
-    if($tab_error === true) {
-      $game_mod->save();
+      if($tab_error === true) {
+        $game_mod->save();
 
-      Page::add_message( 'Game successfuly created' );
+        Page::add_message( __('Game successfuly created') );
+      }else {
+        $html_msg = '';
+        Game::manage_errors( $tab_error, $html_msg );
+        Page::add_message( $html_msg, Page::PAGE_MESSAGE_ERROR );
+      }
     }else {
-      $html_msg = '';
-      Game::manage_errors( $tab_error, $html_msg );
-      Page::add_message( $html_msg, Page::PAGE_MESSAGE_ERROR );
+      Page::add_message( __('You have already created a non-finished game, cancel or finish the first before creating a new one.'), Page::PAGE_MESSAGE_ERROR );
     }
   }
-  
+
   if( $action = getValue('action' ) ) {
     switch( $action ) {
       case 'join' : {
@@ -34,15 +38,91 @@
           $game_to_join = Game::instance( $game_id );
           if( $game_to_join->id ) {
             if( $game_to_join->add_player( $current_player ) ) {
-              Page::add_message('You successfully joined the game !');
+              Page::add_message( __('You successfully joined the game !') );
             }
           }else {
-            Page::add_message('Unknown game', Page::PAGE_MESSAGE_ERROR);
+            Page::add_message( __('Unknown game'), Page::PAGE_MESSAGE_ERROR );
           }
         }else {
-          Page::add_message('Game id parameter missing', Page::PAGE_MESSAGE_ERROR);
+          Page::add_message( __('Game id parameter missing'), Page::PAGE_MESSAGE_ERROR );
         }
         Page::redirect( PAGE_CODE );
+        break;
+      }
+      case 'cancel' : {
+        $game_id = getValue('game_id');
+        if( $game_id ) {
+          /* @var $game_to_delete Game */
+          $game_to_delete = Game::instance( $game_id );
+
+          if( $game_to_delete->id ) {
+
+            if( $game_to_delete->created_by == $current_player->id || is_admin() ) {
+              if( $game_to_delete->started === null ) {
+                $game_player_list = $game_to_delete->get_game_player_list();
+
+                if( $game_to_delete->db_delete() ) {
+                  foreach( $game_player_list as $game_player ) {
+                    if( $current_player->id != $game_player['player_id'] ) {
+                      $player = Player::instance( $game_player['player_id'] );
+                      $member = Member::instance( $player->member_id );
+                      if( php_mail($member->email, SITE_NAME." | Game canceled", $player->get_email_game_cancel( $game_to_delete ), true) ) {
+                        Page::add_message( __("Message sent to %s", $player->name) );
+                      }else {
+                        Page::add_message( __("Message failed to %s", $player->name) , Page::PAGE_MESSAGE_WARNING);
+                        Page::add_message(var_export( error_get_last(), 1 ), Page::PAGE_MESSAGE_WARNING);
+                      }
+                    }
+                  }
+
+                  Page::add_message( __('Game successfuly deleted') );
+                }else {
+                  Page::add_message( __('There was a problem during deletion, please contact an admin'), Page::PAGE_MESSAGE_ERROR );
+                }
+              }else {
+                Page::add_message( __('You can\'t delete a game that has already started'), Page::PAGE_MESSAGE_ERROR );
+              }
+            }else {
+              Page::add_message( __('You can\'t delete a game you\'re not the creator'), Page::PAGE_MESSAGE_ERROR );
+            }
+          }else {
+            Page::add_message( __('Unknown game'), Page::PAGE_MESSAGE_ERROR );
+          }
+        }else {
+          Page::add_message( __('Game id parameter missing'), Page::PAGE_MESSAGE_ERROR );
+        }
+        break;
+      }
+      case 'start' : {
+        $game_id = getValue('game_id');
+        if( $game_id ) {
+          /* @var $game_to_start Game */
+          $game_to_start = Game::instance( $game_id );
+
+          if( $game_to_start->id ) {
+
+            if( $game_to_start->created_by == $current_player->id || is_admin() ) {
+              if( $game_to_start->started === null ) {
+                $game_player_list = $game_to_start->get_game_player_list();
+
+                if( count( $game_player_list ) >= 2 ) {
+                  $game_to_start->start();
+                  Page::add_message( __('Game successfuly started') );
+                }else {
+                  Page::add_message( __('You can\'t start a game with fewer than 2 players'), Page::PAGE_MESSAGE_ERROR );
+                }
+              }else {
+                Page::add_message( __('You can\'t delete a game that has already started'), Page::PAGE_MESSAGE_ERROR );
+              }
+            }else {
+              Page::add_message( __('You can\'t delete a game you\'re not the creator'), Page::PAGE_MESSAGE_ERROR );
+            }
+          }else {
+            Page::add_message( __('Unknown game'), Page::PAGE_MESSAGE_ERROR );
+          }
+        }else {
+          Page::add_message( __('Game id parameter missing'), Page::PAGE_MESSAGE_ERROR );
+        }
         break;
       }
     }
--- data/game/game_list.dsp.php
+++ data/game/game_list.dsp.php
@@ -26,13 +26,15 @@
     <td>
       '.(!$game->started && !$current_game ?'<a href="'.Page::get_url(PAGE_CODE, array('action' => 'join', 'game_id' => $game->id)).'">'.__('Join').'</a>':'').'
       '.($current_game && $current_game->id == $game->id?'<a href="'.Page::get_url('game_over').'">'.__('Quit').'</a>':'').'
+      '.(($current_player->id == $game->created_by || is_admin()) && $game->started === null?'<a href="'.Page::get_url(PAGE_CODE, array('action' => 'cancel', 'game_id' => $game->id)).'">'.__('Cancel').'</a>':'').'
+      '.(($current_player->id == $game->created_by || is_admin()) && $game->started === null?'<a href="'.Page::get_url(PAGE_CODE, array('action' => 'start', 'game_id' => $game->id)).'">'.__('Start').'</a>':'').'
     </td>
   </tr>';
   }
 ?>
 </table>
 <?php
-  if( is_admin() ) {
+  if( $current_player->can_create_game() ) {
 
     echo '
 <form class="formulaire" action="'.Page::get_page_url( PAGE_CODE ).'" method="post">
@@ -40,3 +42,20 @@
   <p>'.HTMLHelper::submit('game_submit', __('Add a game') ).'</p>
 </form>';
   }
+?>
+
+<h4><?php echo __('Wall')?></h4>
+<form action="<?php echo Page::get_url('shout')?>" method="post">
+  <input type="hidden" name="url_return" value="<?php echo Page::get_url(PAGE_CODE)?>" />
+  <p><?php echo '['.guess_time(time(), GUESS_TIME_LOCALE).']'?> <strong><?php echo wash_utf8($current_player->name)?></strong> : <input type="text" name="text" size="80" value=""/><button type="submit" name="action" value="shout">Say</button></p>
+</form>
+<div id="shoutwall">
+<?php
+    $shouts = Shout::db_get_by_game_id( null );
+    foreach( array_reverse( $shouts ) as $shout ) {
+      $player = Player::instance($shout->shouter_id);
+      echo '
+  <div class="shout">['.guess_time($shout->date_sent, GUESS_TIME_LOCALE).'] <strong>'.wash_utf8($player->name).'</strong>: '.wash_utf8($shout->text).'</div>';
+    }
+?>
+</div>
