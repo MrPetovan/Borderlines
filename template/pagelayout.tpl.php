462410d60f493465498d70dca31faf2bac645749
Benjamin Lorteau <ben.lort@gmail.com>
2012-10-15 06:23:39 -0400

    Add current player retrieval Add translation support

--- template/pagelayout.tpl.php
+++ template/pagelayout.tpl.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 <head>
   <title><?php echo SITE_NAME?> - <?php echo wash_utf8($PAGE_TITRE); ?></title>
@@ -10,19 +9,22 @@
     <header>
       <h1><a href="<?php echo URL_ROOT?>"><?php echo SITE_NAME?></a></h1>
       <p class="banner"><a href="http://www.aeriesguard.com/forum/Jeux-video/Borderlines-Alpha-fermee">Open Beta</a></p>
-<?php if(Member::get_current_user_id()) {?>
+<?php if(Member::get_current_user_id()) {
+  $member = Member::get_current_user();
+  $current_player = Player::get_current( $member );
+?>
       <nav role="main">
         <ul>
           <li>[ <?php echo guess_time(time(), GUESS_DATETIME_LOCALE)?> ]</li>
-          <li><a href="<?php echo Page::get_page_url('dashboard') ?>">Play</a></li>
-          <li><a href="<?php echo Page::get_page_url('game_list') ?>">Games</a></li>
+          <li><a href="<?php echo Page::get_page_url('dashboard') ?>"><?php echo __('Play')?></a></li>
+          <li><a href="<?php echo Page::get_page_url('game_list') ?>"><?php echo __('Games')?></a></li>
           <?php if( $unread_count = Message_player::db_get_unread_count($current_player->id) ) :?>
-          <li><a href="<?php echo Page::get_page_url('conversation_list') ?>"><strong>Conversations (<?php echo $unread_count?>)</strong></a></li>
+          <li><a href="<?php echo Page::get_page_url('conversation_list') ?>"><strong><?php echo __('Conversations')?> (<?php echo $unread_count?>)</strong></a></li>
           <?php else:?>
-          <li><a href="<?php echo Page::get_page_url('conversation_list') ?>">Conversations</a></li>
+          <li><a href="<?php echo Page::get_page_url('conversation_list') ?>"><?php echo __('Conversations')?></a></li>
           <?php endif;?>
-          <li><a href="<?php echo Page::get_page_url('mon-compte') ?>">Account</a></li>
-          <li><a href="<?php echo Page::get_page_url('logout') ?>">Logout</a></li>
+          <li><a href="<?php echo Page::get_page_url('mon-compte') ?>"><?php echo __('Account')?></a></li>
+          <li><a href="<?php echo Page::get_page_url('logout') ?>"><?php echo __('Logout')?></a></li>
         </ul>
       </nav>
 <?php }else {?>
@@ -33,13 +35,13 @@
         <p>
           <input type="password" name="pass" value="12345678" onclick="if(this.value = '12345678') this.value = ''"/>
         </p>
-        <button type="submit" name="submit_login" value="Ok">Sign in</button>
+        <button type="submit" name="submit_login" value="Ok"><?php echo __('Sign in')?></button>
       </form>
       <nav role="main">
         <ul>
-          <li><a href="<?php echo Page::get_page_url('accueil') ?>">Home</a></li>
-          <li><a href="<?php echo Page::get_page_url('register') ?>">Sign Up</a></li>
-          <li><a href="<?php echo Page::get_page_url('rappel-identifiants') ?>">Forgotten password ?</a></li>
+          <li><a href="<?php echo Page::get_page_url('accueil') ?>"><?php echo __('Home')?></a></li>
+          <li><a href="<?php echo Page::get_page_url('register') ?>"><?php echo __('Sign Up')?></a></li>
+          <li><a href="<?php echo Page::get_page_url('rappel-identifiants') ?>"><?php echo __('Forgotten password ?')?></a></li>
         </ul>
       </nav>
 <?php } ?>
@@ -62,7 +64,7 @@
             <option value="<?php echo $locale?>"<?php echo $locale == LOCALE?' selected="selected"':''?>><?php echo __($locale)?></option>
             <?php endforeach;?>
           </select>
-          <button type="submit" name="setlocale" value="1">Set</button>
+          <button type="submit" name="setlocale" value="1"><?php echo __('Set')?></button>
         </p>
       </form>
     </footer>
