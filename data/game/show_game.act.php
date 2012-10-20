4f0bb3c2404d52412188aebcbd4a8815be5605c2
Benjamin Lorteau <ben.lort@gmail.com>
2012-10-20 11:56:13 -0400

    Add translation support for show_game page Add sorting by largest territory

--- data/game/show_game.act.php
+++ data/game/show_game.act.php
@@ -6,7 +6,7 @@
   $game = Game::instance( getValue('id') );
 
   if( !$game->id ) {
-    Page::add_message('Unknown game', Page::PAGE_MESSAGE_ERROR);
+    Page::add_message( __('Unknown game'), Page::PAGE_MESSAGE_ERROR);
     Page::redirect('game_list');
   }
 
@@ -14,7 +14,7 @@
     switch( getValue('action') ) {
       case 'join' : {
         if( $game->add_player( $current_player ) ) {
-          Page::add_message('You successfully joined the game !');
+          Page::add_message( __('You successfully joined the game !') );
         }
         break;
       }
@@ -25,7 +25,7 @@
   $game_player_list = $game->get_game_player_list();
   foreach( $game_player_list as $game_player_row ) {
     $player_troops[ $game_player_row['player_id'] ] = 0;
-    $player_area[ $game_player_row['player_id'] ] = 0;
+    $player_area[ 'player_' . $game_player_row['player_id'] ] = 0;
   }
   $territory_player_troops_list = $game->get_territory_player_troops_list($game->current_turn);
   foreach( $territory_player_troops_list as $territory_player_troops_row ) {
@@ -34,10 +34,15 @@
   $territory_owner_list = $game->get_territory_owner_list(null, $game->current_turn);
 
   foreach( $territory_owner_list as $territory_owner_row ) {
-    $territory = Territory::instance( $territory_owner_row['territory_id'] );
-    if( isset( $player_area[ $territory_owner_row['owner_id'] ] )) {
-      $player_area[ $territory_owner_row['owner_id'] ] += $territory->area;
-    }else {
-      $player_area[ $territory_owner_row['owner_id'] ] = $territory->area;
+    if( $territory_owner_row['owner_id'] ) {
+      $territory = Territory::instance( $territory_owner_row['territory_id'] );
+
+      if( isset( $player_area[ 'player_' . $territory_owner_row['owner_id'] ] )) {
+        $player_area[ 'player_' . $territory_owner_row['owner_id'] ] += $territory->area;
+      }else {
+        $player_area[ 'player_' . $territory_owner_row['owner_id'] ] = $territory->area;
+      }
     }
   }
+
+  array_multisort($player_area, SORT_DESC, $game_player_list);
--- data/game/show_game.dsp.php
+++ data/game/show_game.dsp.php
@@ -6,61 +6,63 @@
 <h2><?php echo __('Showing "%s"', $game->name )?></h2>
 <div class="informations formulaire">
   <p class="field">
-    <span class="libelle"><?php echo __('Status')?></span>
-    <span class="value"><?php echo $game->status_string?></span>
+    <span class="label"><?php echo __('Status')?></span>
+    <span class="value"><?php echo __($game->status_string)?></span>
   </p>
   <p class="field">
-    <span class="libelle"><?php echo __('Current Turn')?></span>
+    <span class="label"><?php echo __('Current Turn')?></span>
     <span class="value"><?php echo $game->current_turn.'/'.$game->turn_limit?></span>
   </p>
   <p class="field">
-    <span class="libelle"><?php echo __('Turn Interval')?></span>
-    <span class="value"><?php echo $game->turn_interval?> seconds</span>
+    <span class="label"><?php echo __('Turn Interval')?></span>
+    <span class="value"><?php echo __('%s seconds', $game->turn_interval)?></span>
   </p>
 <?php if( !$game->started && $game->min_players ) { ?>
   <p class="field">
-    <span class="libelle"><?php echo __('Min Players')?></span>
+    <span class="label"><?php echo __('Min Players')?></span>
     <span class="value"><?php echo $game->min_players?></span>
   </p>
 <?php }?>
 <?php if( $game->max_players ) {?>
   <p class="field">
-    <span class="libelle"><?php echo __('Max Players')?></span>
+    <span class="label"><?php echo __('Max Players')?></span>
     <span class="value"><?php echo $game->max_players?></span>
   </p>
 <?php }?>
   <p class="field">
-    <span class="libelle"><?php echo __('Created')?></span>
+    <span class="label"><?php echo __('Created')?></span>
     <span class="value"><?php echo guess_time($game->created, GUESS_DATETIME_LOCALE)?>
     by <a href="<?php echo get_page_url('show_player', true, array('id' => $game->created_by ) )?>"><?php echo $creator->name?></a></span>
   </p>
 <?php if( $game->started ) {?>
   <p class="field">
-    <span class="libelle"><?php echo __('Started')?></span>
+    <span class="label"><?php echo __('Started')?></span>
     <span class="value"><?php echo guess_time($game->started, GUESS_DATETIME_LOCALE)?></span>
   </p>
 <?php }?>
 <?php if( $game->updated && ! $game->ended ) {?>
   <p class="field">
-    <span class="libelle"><?php echo __('Updated')?></span>
+    <span class="label"><?php echo __('Updated')?></span>
     <span class="value"><?php echo guess_time($game->updated, GUESS_DATETIME_LOCALE)?></span>
   </p>
 <?php }?>
 <?php if( $game->ended ) {?>
   <p class="field">
-    <span class="libelle"><?php echo __('Ended')?></span>
+    <span class="label"><?php echo __('Ended')?></span>
     <span class="value"><?php echo guess_time($game->ended, GUESS_DATETIME_LOCALE)?></span>
   </p>
 <?php }elseif( $game->updated ) { ?>
   <p class="field">
-    <span class="libelle"><?php echo __('Next turn')?></span>
+    <span class="label"><?php echo __('Next turn')?></span>
     <span class="value"><?php echo guess_time( $game->updated + $game->turn_interval, GUESS_DATETIME_LOCALE ) ?></span>
   </p>
 <?php }?>
 <?php $world = World::instance($game->world_id);?>
   <p class="field">
-    <span class="libelle"><?php echo __('World')?></span>
-    <span class="value"><a href="<?php echo Page::get_url('show_world', array('game_id' => $game->id))?>"><?php echo $world->name?></a></span>
+    <span class="label"><?php echo __('World')?></span>
+    <span class="value">
+      <a href="<?php echo Page::get_url('show_world', array('game_id' => $game->id))?>"><?php echo $world->name?></a>
+    </span>
   </p>
 </div>
 <h3><?php echo __('Players')?></h3>
@@ -71,6 +73,7 @@
 <table>
   <thead>
     <tr>
+      <th><?php echo __('#')?></th>
       <th><?php echo __('Player')?></th>
       <th><?php echo __('Turn Ready')?></th>
       <th><?php echo __('Controlled territory')?></th>
@@ -79,15 +82,16 @@
   </thead>
   <tfoot>
     <tr>
-      <td colspan="4"><?php echo $game->max_players?__('%s/%s players', count( $game_player_list ), $game->max_players):__('%s players', count( $game_player_list ))?></td>
+      <td colspan="5"><?php echo $game->max_players?__('%s/%s players', count( $game_player_list ), $game->max_players):__('%s players', count( $game_player_list ))?></td>
     </tr>
   </tfoot>
   <tbody>
 <?php
-    foreach( $game_player_list as $game_player_row ) {
+    foreach( $game_player_list as $key => $game_player_row ) {
       $player_id_player = Player::instance( $game_player_row['player_id'] );
       echo '
     <tr>
+      <td class="num">'.($key + 1).'</td>
       <td><a href="'.get_page_url('show_player', true, array('id' => $player_id_player->id)).'">'.$player_id_player->name.'</a></td>';
       if( $game_player_row['turn_leave'] ) {
         echo '
@@ -95,7 +99,7 @@
       }else {
         echo '
       <td class="num">'.$game_player_row['turn_ready'].'</td>
-      <td class="num">'.l10n_number( $player_area[ $game_player_row['player_id'] ] ).' km²</td>
+      <td class="num">'.l10n_number( $player_area[ 'player_' . $game_player_row['player_id'] ] ).' km²</td>
       <td class="num">'.l10n_number( $player_troops[ $game_player_row['player_id'] ] ).' <img src="'.IMG.'img_html/helmet.png" alt="Troops" title="Troops"/></td>';
       }
       echo '
