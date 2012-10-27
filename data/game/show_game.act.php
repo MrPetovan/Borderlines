e665a661b3eed36c6626e09269203f668af94389
Benjamin Lorteau <ben.lort@gmail.com>
2012-10-26 22:21:50 -0400

    Fix incorrect display when player-empty

--- data/game/show_game.act.php
+++ data/game/show_game.act.php
@@ -23,26 +23,28 @@
   }
 
   $game_player_list = $game->get_game_player_list();
-  foreach( $game_player_list as $game_player_row ) {
-    $player_troops[ $game_player_row['player_id'] ] = 0;
-    $player_area[ 'player_' . $game_player_row['player_id'] ] = 0;
-  }
-  $territory_player_troops_list = $game->get_territory_player_troops_list($game->current_turn);
-  foreach( $territory_player_troops_list as $territory_player_troops_row ) {
-    $player_troops[ $territory_player_troops_row['player_id'] ] += $territory_player_troops_row['quantity'];
-  }
-  $territory_owner_list = $game->get_territory_owner_list(null, $game->current_turn);
+  if( count( $game_player_list ) ) {
+    foreach( $game_player_list as $game_player_row ) {
+      $player_troops[ $game_player_row['player_id'] ] = 0;
+      $player_area[ 'player_' . $game_player_row['player_id'] ] = 0;
+    }
+    $territory_player_troops_list = $game->get_territory_player_troops_list($game->current_turn);
+    foreach( $territory_player_troops_list as $territory_player_troops_row ) {
+      $player_troops[ $territory_player_troops_row['player_id'] ] += $territory_player_troops_row['quantity'];
+    }
+    $territory_owner_list = $game->get_territory_owner_list(null, $game->current_turn);
 
-  foreach( $territory_owner_list as $territory_owner_row ) {
-    if( $territory_owner_row['owner_id'] ) {
-      $territory = Territory::instance( $territory_owner_row['territory_id'] );
+    foreach( $territory_owner_list as $territory_owner_row ) {
+      if( $territory_owner_row['owner_id'] ) {
+        $territory = Territory::instance( $territory_owner_row['territory_id'] );
 
-      if( isset( $player_area[ 'player_' . $territory_owner_row['owner_id'] ] )) {
-        $player_area[ 'player_' . $territory_owner_row['owner_id'] ] += $territory->area;
-      }else {
-        $player_area[ 'player_' . $territory_owner_row['owner_id'] ] = $territory->area;
+        if( isset( $player_area[ 'player_' . $territory_owner_row['owner_id'] ] )) {
+          $player_area[ 'player_' . $territory_owner_row['owner_id'] ] += $territory->area;
+        }else {
+          $player_area[ 'player_' . $territory_owner_row['owner_id'] ] = $territory->area;
+        }
       }
     }
-  }
 
-  array_multisort($player_area, SORT_DESC, $game_player_list);
+    array_multisort($player_area, SORT_DESC, $game_player_list);
+  }
