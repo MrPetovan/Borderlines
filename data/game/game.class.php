c59f96e3c349d6c96975c2ab5d5f022a98965a42
Benjamin Lorteau <ben.lort@gmail.com>
2012-10-26 22:24:27 -0400

    Add version default Add ingame player join Add game parameters

--- data/game/game.class.php
+++ data/game/game.class.php
@@ -9,12 +9,15 @@ require_once( DATA."model/game_model.class.php" );
 class Game extends Game_Model {
 
   // CUSTOM
+  protected $_version = 'world';
 
   public function get_parameters() {
     $defaults = array(
         'HOME_TROOPS_MAINTENANCE' => 1,
         'AWAY_TROOPS_MAINTENANCE' => 2,
-        'RECRUIT_TROOPS_PRICE' => 10
+        'RECRUIT_TROOPS_PRICE' => 10,
+        'TROOPS_EFFICACITY' => 10,
+        'ALLOW_JOIN_MIDGAME' => 1,
     );
 
     $options = unserialize($this->_parameters);
@@ -25,6 +28,23 @@ class Game extends Game_Model {
     return $options;
   }
   public function set_parameters($params) { $this->_parameters = serialize($params);}
+
+  public function can_join( Player $player ) {
+    $options = $this->get_parameters();
+    $return = !$this->has_ended();
+
+    if( $return ) {
+      $return = $player->current_game === false;
+    }
+    if( $return && $this->max_players ) {
+      $return = count( $this->get_game_player_list() ) < $this->max_players;
+    }
+    if( $return && $options['ALLOW_JOIN_MIDGAME'] ) {
+      $return = count( $this->get_territory_owner_list(null, $this->current_turn, false) ) >= 5;
+    }
+
+    return $return;
+  }
 
   public function get_status_string() {
     $return = "Waiting for players";
@@ -39,6 +59,31 @@ class Game extends Game_Model {
 
   public function has_ended() {
     return ($this->current_turn >= $this->turn_limit);
+  }
+
+  public function get_territory_owner_list($territory_id = null, $turn = null, $owner_id = null) {
+    $where = '';
+    if( ! is_null( $territory_id )) $where .= '
+AND `territory_id` = '.mysql_ureal_escape_string($territory_id);
+    if( ! is_null( $turn )) $where .= '
+AND `turn` = '.mysql_ureal_escape_string($turn);
+    if( ! is_null( $owner_id )) {
+      if( $owner_id === false ) {
+        $where .= '
+AND `owner_id` IS NULL';
+      }else {
+        $where .= '
+AND `owner_id` = '.mysql_ureal_escape_string($owner_id);
+      }
+    }
+
+    $sql = '
+SELECT `territory_id`, `game_id`, `turn`, `owner_id`, `contested`, `capital`
+FROM `territory_owner`
+WHERE `game_id` = '.mysql_ureal_escape_string($this->get_id()).$where;
+    $res = mysql_uquery($sql);
+
+    return mysql_fetch_to_array($res);
   }
 
   public function reset() {
@@ -71,27 +116,11 @@ class Game extends Game_Model {
 
     $territories = Territory::db_get_by_world_id( $this->world_id );
 
-    shuffle( $territories );
-
     foreach( $player_list as $player ) {
-      foreach( $player_list as $playerB ) {
-        if( $player != $playerB ) {
-          $this->set_player_diplomacy( $this->current_turn, $player->id, $playerB->id, 'Enemy' );
-        }
-      }
-
+      shuffle( $territories );
       $starting_territory = array_pop( $territories );
 
-      $this->set_territory_player_troops($this->current_turn, $starting_territory->id, $player->id, 1000);
-      $this->set_territory_owner($starting_territory->id, $this->current_turn, $player->id, 0, 1);
-
-      $member = Member::instance( $player->member_id );
-      if( php_mail($member->email, SITE_NAME." | Game started", $player->get_email_game_new_turn( $this ), true)) {
-        Page::add_message("Message sent to ".$player->name);
-      }else {
-        Page::add_message("Message failed to ".$player->name, Page::PAGE_MESSAGE_WARNING);
-        Page::add_message(var_export( error_get_last(), 1 ), Page::PAGE_MESSAGE_WARNING);
-      }
+      $this->bootstrap_player( $player, $starting_territory->id );
     }
   }
 
@@ -198,7 +227,7 @@ class Game extends Game_Model {
               $diplomacy[ $diplomacy_row['from_player_id'] ][ $diplomacy_row['to_player_id'] ] = $diplomacy_row['status'] == 'Ally';
             }
             // Battle
-            $attacker_efficiency = (mt_gaussrand() * 0.1 + 1) * 0.1;
+            $attacker_efficiency = 1 / $options['TROOPS_EFFICACITY'];
             $attacker_damages = round($attacker_efficiency * $attacker_row['quantity']);
 
             // Building the attacks directions
@@ -480,6 +509,8 @@ WHERE `ended` IS NULL";
       604800 => "Long - Weekly",
     );
 
+    $options = $this->parameters;
+
     $return = '
     <fieldset>
       <legend>'.__('Create a game !').'</legend>
@@ -490,31 +521,64 @@ WHERE `ended` IS NULL";
       <p class="field">'.HTMLHelper::genererInputText('turn_limit', $this->get_turn_limit(), array('title' => __('Game will stop after a fixed amount of turns')), __('Turn Limit').'*').'</p>
       <p class="field">'.HTMLHelper::genererInputText('min_players', $this->get_min_players(), array('title' => __('Number of players required to automatically launch the game')), __('Minimum nb of players')).'</p>
       <p class="field">'.HTMLHelper::genererInputText('max_players', $this->get_max_players(), array(), __('Maximum nb of players')).'</p>
+    </fieldset>
+    <fieldset>
+      <legend>'.__('Game options').'</legend>
+      <p class="field">'.HTMLHelper::genererInputText('parameters[HOME_TROOPS_MAINTENANCE]', $options['HOME_TROOPS_MAINTENANCE'], array(), __('Home troops cost')).'</p>
+      <p class="field">'.HTMLHelper::genererInputText('parameters[AWAY_TROOPS_MAINTENANCE]', $options['AWAY_TROOPS_MAINTENANCE'], array(), __('Away troops cost')).'</p>
+      <p class="field">'.HTMLHelper::genererInputText('parameters[RECRUIT_TROOPS_PRICE]', $options['RECRUIT_TROOPS_PRICE'], array(), __('Recruit troop price')).'</p>
+      <p class="field">'.HTMLHelper::genererInputText('parameters[TROOPS_EFFICACITY]', $options['TROOPS_EFFICACITY'], array(), __('Troops efficacity (1 damage/x troops)')).'</p>
+      <p class="field">'.HTMLHelper::genererInputCheckBox('parameters[ALLOW_JOIN_MIDGAME]', '0', $options['ALLOW_JOIN_MIDGAME'], array(), __('Allow players to join mid-game')).'</p>
     </fieldset>';
 
     return $return;
   }
 
-  public function add_player( $player ) {
+  public function add_player( Player $player ) {
     $return = false;
 
-    if( !$this->started ) {
-      if( !$player->get_current_game() ) {
-        if( !$this->max_players || count( $this->get_game_player_list() ) < $this->max_players ) {
-          $this->set_game_player( $player->id, -1 );
+    if( $this->can_join( $player ) ) {
+      $this->set_game_player( $player->id, -1 );
+      if( $this->started ) {
+        $empty_territories = $this->get_territory_owner_list(null, $this->current_turn, false);
 
-          $return = true;
-        }else {
-          Page::add_message('Game is already complete', Page::PAGE_MESSAGE_ERROR);
-        }
-      }else {
-        Page::add_message('You are already in a game !', Page::PAGE_MESSAGE_ERROR);
+        shuffle( $empty_territories );
+        $empty_territory = array_pop( $empty_territories );
+
+        $this->bootstrap_player( $player, $empty_territory['territory_id'] );
+        $message = new Shout();
+        $message->game_id = $this->id;
+        $message->shouter_id = $player->id;
+        $message->text = '[joined the game]';
+        $message->date_sent = time();
+        $message->save();
       }
-    }else {
-      Page::add_message('Game already started', Page::PAGE_MESSAGE_ERROR);
+
+      $return = true;
     }
 
     return $return;
+  }
+
+  public function bootstrap_player( Player $player, $territory_id ) {
+    $player_list = Player::db_get_by_game( $this->id );
+    foreach( $player_list as $playerB ) {
+      if( $player != $playerB ) {
+        $this->set_player_diplomacy( $this->current_turn, $player->id, $playerB->id, 'Enemy' );
+        $this->set_player_diplomacy( $this->current_turn, $playerB->id, $player->id, 'Enemy' );
+      }
+    }
+
+    $this->set_territory_player_troops($this->current_turn, $territory_id, $player->id, 1000);
+    $this->set_territory_owner($territory_id, $this->current_turn, $player->id, 0, 1);
+
+    $member = Member::instance( $player->member_id );
+    if( php_mail($member->email, SITE_NAME." | Game started", $player->get_email_game_new_turn( $this ), true)) {
+      Page::add_message("Message sent to ".$player->name);
+    }else {
+      Page::add_message("Message failed to ".$player->name, Page::PAGE_MESSAGE_WARNING);
+      Page::add_message(var_export( error_get_last(), 1 ), Page::PAGE_MESSAGE_WARNING);
+    }
   }
 
   // /CUSTOM
