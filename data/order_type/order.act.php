6b25b84be96f0d4f06ff37e10b2075ff5a933dde
Benjamin Lorteau <ben.lort@gmail.com>
2012-11-18 05:17:42 +0100

    Fix current_player initialization

--- data/order_type/order.act.php
+++ data/order_type/order.act.php
@@ -1,7 +1,6 @@
 <?php
   $member = Member::instance( Member::get_current_user_id() );
-  $player_list = Player::db_get_by_member_id( $member->get_id() );
-  $current_player = array_shift( $player_list );
+  $current_player = Player::get_current( $member );
 
   if( $action = getValue('action') ) {
     if( $action == "cancel" ) {
