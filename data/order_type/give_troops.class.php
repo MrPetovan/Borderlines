65a41a21f931e07a35252238c59d04b73094ec3f
Benjamin Lorteau <ben.lort@gmail.com>
2012-12-05 23:33:15 -0500

    Add order icon to order form

--- data/order_type/give_troops.class.php
+++ data/order_type/give_troops.class.php
@@ -140,32 +140,34 @@ class Give_Troops extends Player_Order {
     $return = '
 <form action="'.Page::get_page_url( 'order' ).'" method="post">
   <fieldset>
-    <legend>'.$title.'</legend>';
+    <legend><img src="'.IMG.'img_html/troops_gift.png" alt="" /> '.$title.'</legend>
+    <div class="content">';
     if( !isset( $params['from_territory'] ) || count( $territory_select ) != 0 ) {
       $return .= HTMLHelper::genererInputHidden('url_return', Page::get_page_url( $params['page_code'], true, $page_params ) ).'
-    <p>'.HTMLHelper::genererInputText( 'parameters[count]', 0, array(), 'Troop size', null ).'</p>';
+      <p>'.HTMLHelper::genererInputText( 'parameters[count]', 0, array(), 'Troop size', null ).'</p>';
       if( isset( $params['from_territory'] ) ) {
         $return .= '
-    '.HTMLHelper::genererInputHidden('parameters[from_territory_id]', $params['from_territory']->id);
+      '.HTMLHelper::genererInputHidden('parameters[from_territory_id]', $params['from_territory']->id);
       }else {
         $return .= '
-    <p>'.HTMLHelper::genererSelect( 'parameters[from_territory_id]', $territory_select, null, array(), 'Give from' ).'</p>';
+      <p>'.HTMLHelper::genererSelect( 'parameters[from_territory_id]', $territory_select, null, array(), 'Give from' ).'</p>';
       }
       if( isset( $params['to_player'] ) ) {
         $return .= '
-    '.HTMLHelper::genererInputHidden('parameters[to_player_id]', $params['to_player']->id);
+      '.HTMLHelper::genererInputHidden('parameters[to_player_id]', $params['to_player']->id);
       }else {
         $return .= '
-    <p>'.HTMLHelper::genererSelect( 'parameters[to_player_id]', $player_select, null, array(), 'Give to' ).'</p>';
+      <p>'.HTMLHelper::genererSelect( 'parameters[to_player_id]', $player_select, null, array(), 'Give to' ).'</p>';
       }
 
     $return .= '
-    <p>'.HTMLHelper::genererButton( 'action', strtolower(__CLASS__), array('type' => 'submit'), "March !" ).'</p>';
+      <p>'.HTMLHelper::genererButton( 'action', strtolower(__CLASS__), array('type' => 'submit'), "March !" ).'</p>';
     }else {
       $return .= '
-    <p>You don\'t have any troops to give</p>';
+      <p>You don\'t have any troops to give</p>';
     }
     $return .= '
+    </div>
   </fieldset>
 </form>';
 
